03064e51c778c4343bc12723901009c397e0b6a1
Kelly Buckley <princesskells@Kellys-MacBook-Air.local>
2023-08-16 19:05:34 -0700

    adding data

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-const express = require("express")
+const express = require("express");
+
+const PORT = 8080;
+const app = express()
+
+app.get('/', (req, res)=>{
+    res.send("Hello")
+});
+
+app.listen(PORT, ()=>{
+    console.log(`server is running on ${PORT} successfully`)
+});
